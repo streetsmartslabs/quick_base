381fda9b8185e94177bafb7c31db906b53c50249
Corey Psoinos <corey@streetsmarts.io>
2020-10-02 16:53:43 -0400

    change URI.encode to URI.parse

--- lib/quick_base/configuration.rb
+++ lib/quick_base/configuration.rb
@@ -175,7 +175,7 @@ module QuickBase
 
     def base_url
       url = "#{scheme}://#{[host, base_path].join('/').gsub(/\/+/, '/')}".sub(/\/+\z/, '')
-      URI.encode(url)
+      URI.parse(url).to_s
     end
 
     # Gets API key (with prefix if set).
